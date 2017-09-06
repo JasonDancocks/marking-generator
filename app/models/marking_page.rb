2ffa5a1ccce9edab57a78dbead8824c199b3da42
Jason Dancocks <jason.dancocks@gmail.com>
2017-09-06 18:06:27 +0100

    add drop downs for existing p_statements

--- app/models/marking_page.rb
+++ app/models/marking_page.rb
@@ -2,4 +2,7 @@ class MarkingPage < ApplicationRecord
 	has_many :p_statements
 
 	accepts_nested_attributes_for :p_statements
+
+
 end
+
--- app/models/p_statement.rb
+++ app/models/p_statement.rb
@@ -1,3 +1,4 @@
 class PStatement < ApplicationRecord
 	has_and_belongs_to_many :marking_pages
+
 end
