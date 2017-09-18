ac76b077f51549dd1cb6e95f6ae2aa4cc8ea0c03
Jason Dancocks <jason.dancocks@gmail.com>
2017-09-18 18:07:34 +0100

    addprint css rules and custom sheet amount

--- app/models/marking_page.rb
+++ app/models/marking_page.rb
@@ -1,6 +1,8 @@
 class MarkingPage < ApplicationRecord
 	has_many :p_statements
 
+	validates :count, numericality: { only_integer: true}
+
 	accepts_nested_attributes_for :p_statements
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170905194523) do
+ActiveRecord::Schema.define(version: 20170918162451) do
 
   create_table "i_statements", force: :cascade do |t|
     t.text "statement"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170905194523) do
   create_table "marking_pages", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "count"
   end
 
   create_table "p_statements", force: :cascade do |t|
