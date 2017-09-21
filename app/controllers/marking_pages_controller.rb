71932adec171adb593c4ff3fad6830d787352876
Jason Dancocks <jason.dancocks@gmail.com>
2017-09-21 21:08:54 +0100

    improve styling and print flow, finished index page

--- app/controllers/marking_pages_controller.rb
+++ app/controllers/marking_pages_controller.rb
@@ -10,6 +10,7 @@ class MarkingPagesController < ApplicationController
 
 	def create
 		@marking_page = MarkingPage.new(marking_page_params)
+		remove_blanks(@marking_page)
 		if @marking_page.save
 			redirect_to @marking_page
 		else
@@ -30,11 +31,20 @@ class MarkingPagesController < ApplicationController
 
 	def print
 		@marking_page = MarkingPage.find(params[:marking_page_id])
+		render layout: "print"
 	end
 
 	private
 
 	def marking_page_params
 		params.require(:marking_page).permit( p_statements_attributes: [:id, :branch,    :statement, :i_statement])
+	end
+
+	def remove_blanks(marking_page)
+		marking_page.p_statements.each do |p|
+			if p.statement.empty?
+				p.delete
+			end
+		end
 	end
 end
