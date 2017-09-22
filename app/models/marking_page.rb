78ccfcd974e03af41c6ff1e36db839e136fe6cc1
Jason Dancocks <jason.dancocks@gmail.com>
2017-09-22 11:14:08 +0100

    Update marking_page.rb

--- app/models/marking_page.rb
+++ app/models/marking_page.rb
@@ -1,6 +1,6 @@
 class MarkingPage < ApplicationRecord
 	has_many :p_statements
-	default_scope { order(created_at: :desc) }
+	scope :bank_feed, ->{ order(created_at: :desc) }
 	accepts_nested_attributes_for :p_statements
 
 
